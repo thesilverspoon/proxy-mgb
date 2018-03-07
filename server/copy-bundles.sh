c52099ee2a934c51b35831ffa1b86fbb551c294e
Mat Bagnall <matbagnall@gmail.com>
2018-03-07 13:46:36 -0800

    added .sh scripts to copy bundles

---
+++ server/copy-bundles.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+cp ./about-photos-component/client/dist/bundle.js ./dist/about-bundle.js
+cp ./about-photos-component/client/styles.css ./dist/about-styles.css
+
+cp ./reservations-component/client/dist/bundle-prod.js ./dist/res-bundle.js
+
+cp ./reviews-component/react/dist/reviews.css ./dist/reviews-reviews.css
+cp ./reviews-component/react/dist/bundle-prod.js ./dist/reviews-bundle.js
+
+cp ./menu-component/src/public/bundle-prod.js ./dist/menu-bundle.js
